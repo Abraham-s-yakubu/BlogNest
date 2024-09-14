791ea221551dec2fb99acdda79e78fe12e650dda
Abraham <yakubu.s.abraham@gmail.com>
2024-09-14 21:20:18 +0100

    changes to setting

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -139,7 +139,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Email settings for Django
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == 'True'
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
