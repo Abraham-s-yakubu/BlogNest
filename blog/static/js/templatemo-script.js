6314d7f5ca95e01740af71108fc0c5ff33cfc6de
Abraham <yakubu.s.abraham@gmail.com>
2024-09-17 14:36:28 +0100

    changes to the js

--- blog/static/js/templatemo-script.js
+++ blog/static/js/templatemo-script.js
@@ -1,18 +1,22 @@
+icon =  document.querySelector(".nav-icon-btn")
 $(function() {
     $(".navbar-toggler-main").on("click", function(e) {
         $(".tm-header").toggleClass("show");
+        icon.classList.toggle("fa-times");
         e.stopPropagation();
       });
     
       $("html").click(function(e) {
         var header = document.getElementById("tm-header");
-    
+
         if (!header.contains(e.target)) {
+          icon.classList.remove("fa-times");
           $(".tm-header").removeClass("show");
         }
       });
-    
+
       $("#tm-nav .nav-link").click(function(e) {
+      icon.classList.remove("fa-times");
         $(".tm-header").removeClass("show");
       });
 });
